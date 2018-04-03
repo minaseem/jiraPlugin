0f7933618c1af4be01fa2fea2ebb929838e61697
Maciej Nowakowski <mnowakowski@atlassian.com>
2018-04-03 13:59:16 +0200

    add component import

--- src/main/java/com/atlassian/flock/rest/MyRestResource.java
+++ src/main/java/com/atlassian/flock/rest/MyRestResource.java
@@ -1,8 +1,8 @@
 package com.atlassian.flock.rest;
 
 import com.atlassian.flock.components.MYComponentImpl;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
-import com.atlassian.sal.api.pluginsettings.PluginSettings;
 import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,11 +16,14 @@ import javax.ws.rs.core.Response;
 @Path("/flock")
 public class MyRestResource {
     private final MYComponentImpl myComponent;
+    private final PluginSettingsFactory pluginSettingsFactory;
 
     @Autowired
-    public MyRestResource(MYComponentImpl myComponent) {
+    public MyRestResource(MYComponentImpl myComponent, @ComponentImport PluginSettingsFactory pluginSettingsFactory) {
         this.myComponent = myComponent;
+        this.pluginSettingsFactory = pluginSettingsFactory;
     }
+
     @GET
     @AnonymousAllowed
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
